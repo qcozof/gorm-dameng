6e21f5711bdc6c36323c1116013b1615de0dfdd2
q.wu <wq@adcinfo.cn>
2021-03-16 16:50:56 +0800

    init

---
+++ demo.go
@@ -0,0 +1,64 @@
+/**
+ * @Description $
+ * @Author $
+ * @Date $ $
+ **/
+package main
+
+import (
+	"fmt"
+	dm8 "github.com/qcozof/gorm-dm8"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"os"
+	"time"
+)
+
+func main()  {
+
+	fmt.Println("连接dameng...")
+	gormConfig := config(true)
+	GORM_DB, err := gorm.Open(dm8.Open( "dm://sysdba:dameng123!@193.100.100.221:5236?autoCommit=true"), gormConfig)
+
+	if err != nil {
+		fmt.Println("连接失败：", err)
+		os.Exit(0)
+	}
+	fmt.Println("连接成功：",GORM_DB)
+}
+
+
+
+func config(logMode bool) (c *gorm.Config) {
+	if logMode {
+		c = &gorm.Config{
+			Logger: logger.New(log.New(os.Stdout, "\r\n", log.LstdFlags), logger.Config{
+				SlowThreshold: 1 * time.Millisecond,
+				LogLevel:      logger.Warn,
+				Colorful:      true,
+			}),
+			DisableForeignKeyConstraintWhenMigrating: true,
+			NamingStrategy: schema.NamingStrategy{
+				TablePrefix:"DW.",
+				SingularTable: true, //表名后面不加s
+			},
+
+		}
+	} else {
+		c = &gorm.Config{
+			Logger:                                   logger.Default.LogMode(logger.Silent),
+			DisableForeignKeyConstraintWhenMigrating: true,
+			NamingStrategy: schema.NamingStrategy{
+				TablePrefix:"DW.",
+				SingularTable: true, //表名后面不加s
+
+			},
+			//Namer.ColumnName : func() {
+			//
+			//},
+		}
+	}
+	return
+}
