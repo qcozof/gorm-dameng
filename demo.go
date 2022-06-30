799c19b7ad57c20cfc8b5a43d7235e9c0996f552
cozof <q@cozof>
2022-06-30 11:04:10 +0800

    fmt

--- demo.go
+++ demo.go
@@ -1,7 +1,5 @@
 /**
  * @Description 操作oracle和dameng
- * @Author $
- * @Date $ $
  **/
 package main
 
@@ -26,15 +24,15 @@ func main() {
 	var err error
 	var GORM_DB *gorm.DB
 
-	fmt.Println(fmt.Printf("连接%s...",dbType))
+	fmt.Println(fmt.Printf("连接%s...", dbType))
 	gormConfig := config(true)
 
-	if dbType == "dameng"{
-		GORM_DB, err = gorm.Open(dameng.Open("dm://sysdba:dameng123!@193.100.100.221:5236?autoCommit=true"), gormConfig)
-	}else if dbType == "oracle"{
-		GORM_DB, err = gorm.Open(oracle.Open("ZTK/sirc1234@193.100.100.43:1521/ORCL"), gormConfig)
-		//GORM_DB, err := gorm.Open(dameng.Open( "dm://sysdba:dameng123!@193.100.100.221:5236?autoCommit=true&ignoreCase=true&columnNameUpperCase=true&compatibleMode=oracle"), gormConfig)
-	}else{
+	if dbType == "dameng" {
+		GORM_DB, err = gorm.Open(dameng.Open("dm://sysdba:dameng123456!@192.168.1.200:5236?autoCommit=true"), gormConfig)
+	} else if dbType == "oracle" {
+		GORM_DB, err = gorm.Open(oracle.Open("DBUSER1/ora123456@192.168.1.200:1521/ORCL"), gormConfig)
+		//GORM_DB, err := gorm.Open(dameng.Open( "dm://sysdba:dameng123456!@192.168.1.200:5236?autoCommit=true&ignoreCase=true&columnNameUpperCase=true&compatibleMode=oracle"), gormConfig)
+	} else {
 		fmt.Println("dbType不正确")
 		os.Exit(0)
 	}
@@ -45,71 +43,72 @@ func main() {
 	}
 	fmt.Println("连接成功：", GORM_DB)
 
-	/*	err = GORM_DB.Exec("update  DW.MEMBER_INFO set MANAGERANGE='吸收公众存款;发放短期、中期和长期贷款;办理国内结算;办理票据承兑与贴现;代理发行、代理兑付、承销政府债券;买卖政府债券、金融债券;从事同业拆借;办理借记卡、贷记卡业务;代理收付款项及代理保险业务;办理外汇存款、外汇贷款、外汇汇款、外币兑换、国际结算、同业外汇存放、拆借业务及资信调查、咨询、见证业务;经银行业监督管理机构批准的其他业务。(依法须经批准的项目,经相关部门批准后方可开展经营活动)' WHERE MEMBER_ID='C3DDBD2F17554E8A838DB706C139B883' ").Error
+	/*	err = GORM_DB.Exec("update  DW.COMPANY_INFO set MANAGE_RANGE='吸收公众存款;发放短期、中期和长期贷款;办理国内结算;办理票据承兑与贴现;代理发行、代理兑付、承销政府债券;买卖政府债券、金融债券;从事同业拆借;办理借记卡、贷记卡业务;代理收付款项及代理保险业务;办理外汇存款、外汇贷款、外汇汇款、外币兑换、国际结算、同业外汇存放、拆借业务及资信调查、咨询、见证业务;经银行业监督管理机构批准的其他业务。(依法须经批准的项目,经相关部门批准后方可开展经营活动)' WHERE COMPANY_ID='C3DDBD2F17554E8A838DB706C139B883' ").Error
 		if err != nil{
 			fmt.Errorf("err:",err)
 		}*/
 
-	/*	var memberInfo MemberInfo
-		err = GORM_DB.Raw("SELECT MEMBER_ID,CNNAME,MANAGERANGE FROM DW.MEMBER_INFO WHERE MEMBER_ID='C3DDBD2F17554E8A838DB706C139B883' ").Scan(&memberInfo).Error //MANAGERANGE
+	/*	var companyInfo CompanyInfo
+		err = GORM_DB.Raw("SELECT COMPANY_ID,CN_NAME,MANAGE_RANGE FROM DW.COMPANY_INFO WHERE COMPANY_ID='C3DDBD2F17554E8A838DB706C139B883' ").Scan(&companyInfo).Error //MANAGE_RANGE
 		if err != nil{
 			fmt.Errorf("err:",err)
 		}
 
 
 		fmt.Println("----------------------")
-		fmt.Println(fmt.Printf("%v",memberInfo.ManageRange))
+		fmt.Println(fmt.Printf("%v",companyInfo.ManageRange))
 		fmt.Println("----------------------")
 
-		tmp := 	memberInfo.ManageRange
+		tmp := 	companyInfo.ManageRange
 		le,err := tmp.GetLength()
 		manageRange,err := tmp.ReadString(1,int(le))
 
 		fmt.Println("str:",manageRange)*/
 
 	//-----------------------------------------
-	var memberList MemberInfo
-	//err = GORM_DB.Raw("SELECT MEMBER_ID,CNNAME,MANAGERANGE FROM DW.MEMBER_INFO WHERE MEMBER_ID in('C3DDBD2F17554E8A838DB706C139B883') ").Scan(&memberList).Error //MANAGERANGE ,'4A5A6EA9B47445D48CB30683BEE68C4A'
-	//err = GORM_DB.Raw("select t.ID,t.Title,t.Content,t.rowid from dw.table1 t where id=1 ").Scan(&memberList).Error //MANAGERANGE ,'4A5A6EA9B47445D48CB30683BEE68C4A'
+	var companyList []CompanyInfo
+	//err = GORM_DB.Raw("SELECT COMPANY_ID,CN_NAME,MANAGE_RANGE FROM DW.COMPANY_INFO WHERE COMPANY_ID in('C3DDBD2F17554E8A838DB706C139B883') ").Scan(&companyList).Error //MANAGE_RANGE ,'4A5A6EA9B47445D48CB30683BEE68C4A'
+	//err = GORM_DB.Raw("select t.ID,t.Title,t.Content,t.rowid from dw.table1 t where id=1 ").Scan(&companyList).Error //MANAGE_RANGE ,'4A5A6EA9B47445D48CB30683BEE68C4A'
 
 	// 读取
-	GORM_DB.Select("MEMBER_ID,CNNAME").Where("MEMBER_ID = ?", "C3DDBD2F17554E8A838DB706C139B883").Find(&memberList) // 查询id为1的product
-	//GORM_DB.First(&memberList, "MEMBER_ID = ?", "C3DDBD2F17554E8A838DB706C139B883").Select("ID") // 查询code为l1212的product
+	GORM_DB.Select("COMPANY_ID,CN_NAME").Where("COMPANY_ID = ?", "C3DDBD2F17554E8A838DB706C139B888").Find(&companyList) // 查询id为1的product
+	//GORM_DB.First(&companyList, "COMPANY_ID = ?", "C3DDBD2F17554E8A838DB706C139B883").Select("ID") // 查询code为l1212的product
 	if err != nil {
 		fmt.Println("err:", err)
 		os.Exit(0)
 	}
 
-/*	err = GORM_DB.Exec("update  dw.table1 set CONTENT=? where ID=2 ").Error
-	if err != nil {
-		fmt.Println("err:", err)
-		os.Exit(0)
-	}*/
+	/*	err = GORM_DB.Exec("update  dw.table1 set CONTENT=? where ID=2 ").Error
+		if err != nil {
+			fmt.Println("err:", err)
+			os.Exit(0)
+		}*/
 
-/*	err = GORM_DB.Exec("insert into  dw.table1(tiTle,conTENt)values('标题6','内容6') ").Error
-	//err = GORM_DB.Exec("update  dw.table1 set title='标题333',content='内容333' where id=3").Error
-	if err != nil {
-		fmt.Println("err:", err)
-		os.Exit(0)
-	}*/
+	/*	err = GORM_DB.Exec("insert into  dw.table1(tiTle,conTENt)values('标题6','内容6') ").Error
+		//err = GORM_DB.Exec("update  dw.table1 set title='标题333',content='内容333' where id=3").Error
+		if err != nil {
+			fmt.Println("err:", err)
+			os.Exit(0)
+		}*/
 
-	fmt.Println(memberList)
+	fmt.Println(companyList)
 }
 
 //column名与增删改查的字段名大小写要保持一致，否则取不到
-/*type MemberInfo struct {
+/*type CompanyInfo struct {
 	Id      string              `gorm:"column:ID"`
 	Title   string              `gorm:"column:TITLE"`
 	Content customdbtype.MyClob `gorm:"column:CONTENT"`
 }*/
 
-func (MemberInfo) TableName() string {
-	return "dw.MEMBER_INFO"
+func (CompanyInfo) TableName() string {
+	return "dd.COMPANY_INFO"
 }
-type MemberInfo struct {
-	MemberId    string              `gorm:"column:MEMBER_ID"`
-	CnName      string              `gorm:"column:CNNAME"`
-	ManageRange customdbtype.MyClob `gorm:"column:MANAGERANGE"`
+
+type CompanyInfo struct {
+	CompanyId   string              `gorm:"column:COMPANY_ID"`
+	CnName      string              `gorm:"column:CN_NAME"`
+	ManageRange customdbtype.MyClob `gorm:"column:MANAGE_RANGE"`
 }
 
 func config(logMode bool) (c *gorm.Config) {
@@ -122,17 +121,16 @@ func config(logMode bool) (c *gorm.Config) {
 			}),
 			DisableForeignKeyConstraintWhenMigrating: true,
 			NamingStrategy: schema.NamingStrategy{
-				TablePrefix:"DW.",
+				TablePrefix:   "DD.",
 				SingularTable: true, //表名后面不加s
 			},
-
 		}
 	} else {
 		c = &gorm.Config{
 			Logger:                                   logger.Default.LogMode(logger.Silent),
 			DisableForeignKeyConstraintWhenMigrating: true,
 			NamingStrategy: schema.NamingStrategy{
-				TablePrefix:"DW.",
+				TablePrefix:   "DD.",
 				SingularTable: true, //表名后面不加s
 			},
 			//Namer.ColumnName : func() {
